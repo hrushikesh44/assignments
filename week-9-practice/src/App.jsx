aa06db67f1963bd0967b8623e1abdb9443d9ccb5
hrushikesh44 <hrushikeshmodupalli12345@gmail.com>
2025-01-13 03:16:37 +0530

    profile card

--- week-9-practice/src/App.jsx
+++ week-9-practice/src/App.jsx
@@ -4,21 +4,33 @@ function App() {
   return (
     <div style={{background: "#dfe6e9", height: "100vh", margin: 0,
        display: "flex",
-       justifyContent: "center"
-    }}>
-      <div style={{marginTop: 10 }}>
-        <div>
-          <PostComponent/>
+       justifyContent: "space-evenly"
+      }}>
+        <div style={{display: "flex", marginTop: 25}}>
+          <div>
+            <ProfileCard/>
+          </div>
+          <div style={{marginTop: 10 }}>
+              <div>
+                <PostComponent/>
+              </div>
+              <br/>
+              <div>
+                <PostComponent/>
+              </div>
+              <br/>
+              <div>
+                <PostComponent/>
+              </div>
+              <br/>
+              <div>
+                <PostComponent/>
+              </div>
+          </div>
+          <div>
+            <ProfileCard/>
+          </div>
         </div>
-        <br/>
-        <div>
-          <PostComponent/>
-        </div>
-        <br/>
-        <div>
-          <PostComponent/>
-        </div>
-      </div>
     </div>
   )
 }
@@ -50,5 +62,33 @@ function PostComponent(){
     </div>
   )
 }
+
+function ProfileCard(){
+  return(
+    <>
+      <div style={{background: "white", borderRadius: 15, margin: 15, minWidth: 400, minHeight: 300,
+      }}>
+        <div style={{display:"flex"}}>
+          <div>
+            <img src="https://pluspng.com/img-png/pitbull-png-junior-the-pitbull-also-known-as-junior-is-one-of-cesar-millan-s-dogs-that-appears-in-the-dog-whisperer-with-cesar-millan-he-is-the-successor-of-daddy-the-300.png"
+            style={{width: 200,
+              height: 150, 
+              borderRadius: 100,
+              }}
+            />
+          </div>
+          <div style={{fontSize: 20}}>
+            <b>Hrushikesh Modupalli</b>
+          </div>
+        </div>
+        <div style={{fontSize: 24}}>
+        I like to talk about dogs
+        </div>
+      </div>
+    </>
+  )
+}
+
+
 
 export default App
