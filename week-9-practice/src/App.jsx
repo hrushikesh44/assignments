68c3c3bf1beb910d6e582bbc18aadf89f8dc8d23
hrushikesh44 <hrushikeshmodupalli12345@gmail.com>
2025-01-13 01:32:51 +0530

    static react SPA

--- week-9-practice/src/App.jsx
+++ week-9-practice/src/App.jsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{background: "#dfe6e9", height: "100vh", margin: 0,
+       display: "flex",
+       justifyContent: "center"
+    }}>
+      <div style={{marginTop: 10 }}>
+        <div>
+          <PostComponent/>
+        </div>
+        <br/>
+        <div>
+          <PostComponent/>
+        </div>
+        <br/>
+        <div>
+          <PostComponent/>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    </div>
+  )
+}
+
+const style = { width: 200,display:"flex", borderRadius: 10, borderWidth: 1, borderColor: "gray",  }
+
+function PostComponent(){
+  return(
+    <div style={{maxWidth: 500, borderRadius: 3 , background: "white",
+       borderRadius: 10, padding: 10
+    }}>
+      <div  style={style}>
+        <div>
+          <img src={"https://pluspng.com/img-png/pitbull-png-junior-the-pitbull-also-known-as-junior-is-one-of-cesar-millan-s-dogs-that-appears-in-the-dog-whisperer-with-cesar-millan-he-is-the-successor-of-daddy-the-300.png"} 
+          style={{width: 70,
+            height: 60,
+            borderRadius: 20
+          }}/>
+        </div>
+        <div style={{fontSize: 18, }}>
+          <b>Hrushikesh</b>
+          <div>12000 followers</div>
+          <div>13h ago</div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div style={{fontSize: 24}}>
+        Want to know about pitbulls.. join the community of around 10,000 dog parents
+      </div>
+    </div>
   )
 }
 
