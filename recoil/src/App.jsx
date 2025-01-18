9f9d139e5b41cdaba3afcaf46ec6ee6cbd6fd8f0
hrushikesh44 <hrushikeshmodupalli12345@gmail.com>
2025-01-19 00:57:23 +0530

    recoil

--- recoil/src/App.jsx
+++ recoil/src/App.jsx
@@ -1,50 +1,51 @@
-import { Children, createContext, useContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+import { counterAtom } from './store/atoms/counterAtom'
 
-const CountContext = createContext();
-
-function CountProvider({ children }){
-  const [count, setCount] = useState(0);
-
-
-  return <CountContext.Provider value={{count, setCount }} >
-    {children}
-  </CountContext.Provider >
-}
 
 function Decrease() {
-  const {count, setCount} = useContext(CountContext);
-  return <button onClick={() => setCount(count - 1)}>Decrease</button>;
+  const setCount = useSetRecoilState(counterAtom)
+
+  function decrease(){
+    setCount(c => c - 1);
+  }
+
+  return <button onClick={decrease}>Decrease</button>;
 }
 
 function Increase() {
-  const {count, setCount} = useContext(CountContext);
-  return <button onClick={() => setCount(count + 1)}>Increase</button>;
+  const setCount = useSetRecoilState(counterAtom)
+
+  function increase(){
+    setCount(c => c + 1)
+  }
+
+  return <button onClick={increase}>Increase</button>;
 }
 
 function Value() {
-  const {count} = useContext(CountContext);
+  const count = useRecoilValue(counterAtom)
+
   return <p>Count: {count}</p>;
 }
 
-function Component() {
+function Counter() {
 
   return <div>
-    <CountProvider>
       <Increase />
       <Decrease />
       <Value />
-      </CountProvider>
   </div>
 }
-
 
 function App() {
 
   return <div>
-      <Component /> 
+    <RecoilRoot>
+      <Counter /> 
+    </RecoilRoot>
   </div>
 }
 
