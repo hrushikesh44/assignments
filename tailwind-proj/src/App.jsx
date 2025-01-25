4c8b0bec621a73c301df021bb1fd2fdcfeef5acd
hrushikesh44 <hrushikeshmodupalli12345@gmail.com>
2025-01-26 02:32:43 +0530

    responsive

--- tailwind-proj/src/App.jsx
+++ tailwind-proj/src/App.jsx
@@ -5,17 +5,25 @@ import { Profilecard } from './Profilecard'
 
 function App() {
 
-  return (
-    <div className='flex'>
-      <Sidebar />
-      <MainComponent />
-    </div>
+  return (<>
+  <MobileTopbar />
+      <div className='flex'>
+        <Sidebar />
+        <MainComponent />
+      </div>
+    </>
   )
 }
 
 function Sidebar(){
   
-  return <div className='bg-slate-200 h-screen w-[25vw] '>
+  return <div className='bg-slate-200 h-screen w-[25vw] hidden lg:block'>
+
+  </div>
+}
+
+function MobileTopbar(){
+  return <div className='bg-slate-200 h-[10vh] w-screen block md:hidden '>
 
   </div>
 }
@@ -23,17 +31,17 @@ function Sidebar(){
 function MainComponent() {
 
   return <div className='w-full '>
-    <div className='bg-slate-300 h-[25vh] '>
+    <div className='bg-slate-300 h-[25vh] hidden md:block'>
 
     </div>
     <div className=' grid grid-cols-12 gap-4 mt-2.5'>
-      <div className='bg-slate-200 rounded-2xl h-96 col-span-3 shadow-lg -translate-y-20 ml-3.5 hidden md:block transition-all duration-700 '>
+      <div className='bg-slate-200 rounded-2xl h-96 col-span-3 shadow-lg -translate-y-20 ml-3.5 hidden lg:block'>
         <Profilecard />
       </div>
-      <div className='bg-slate-200 rounded-2xl h-[50vh] col-span-6 shadow-lg mt-20' >
+      <div className='bg-slate-200 rounded-2xl h-[50vh] col-span-11  lg:col-span-6 md:col-span-8 shadow-lg mt-20 ml-10 md:ml-2.5 ' >
 
       </div>
-      <div className='bg-slate-200 rounded-2xl h-96 col-span-3 shadow-lg mt-10 mr-10 flex justify-around'>
+      <div className='bg-slate-200 rounded-2xl h-96 col-span-11 lg:col-span-3 md:col-span-4 shadow-lg mt-10 mr-4 md:mr-4 ml-10 md:ml-2.5 flex justify-around mb-10 md:mb-0'>
             <AddMeeting />
             <Schedule />
 
--- tailwind-proj/src/Profilecard.jsx
+++ tailwind-proj/src/Profilecard.jsx
@@ -2,18 +2,15 @@
 export function Profilecard () {
 
     return <>
-        <div className=" grid grid-cols-2 content-center">
+        <div className="flex flex-col justify-center items-center mt-10 ">
             <img src="https://images.pexels.com/photos/825947/pexels-photo-825947.jpeg?auto=compress&cs=tinysrgb&w=600"
              alt="profile" className="w-[150px] h-[150px] rounded-2xl shadow-2xl" />
-        </div>
-        <br />
-        <div className="mt-[-100px] grid place-items-center pt-24">
-            <span className="text-2xl font-bold">Hrushikesh Modupalli</span>
+            <span className=" font-bold">Hrushikesh Modupalli</span>
             <br/>
-            <span className="text-xl font-semibold text-gray-500">hrushikesh@gmail.com</span>
-            <span className="text-xl font-semibold text-gray-500">9999988888</span>
+            <span className="text-lg md:text-xl font-semibold text-gray-500">hrushikesh@gmail.com</span>
+            <span className="text-lg md:text-xl font-semibold text-gray-500">9999988888</span>
             <br/>
-            <span className="text-lg font-semibold text-gray-500">Chennai, India</span>
+            <span className="text-lg md:text-xl font-semibold text-gray-500 pb-2">Chennai, India</span>
         </div>
     </>
 }
